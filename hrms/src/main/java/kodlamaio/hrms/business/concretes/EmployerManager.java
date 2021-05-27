4060524546466290b5c1b3b14c2d2ed93aeeb29a
Vahdet Parlak <vahdetparlak@gmail.com>
2021-05-27 20:28:48 +0300

    Commit2

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/EmployerManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/EmployerManager.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.EmployerService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.ErrorResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessResult;
@@ -16,8 +17,8 @@ import kodlamaio.hrms.entities.concretes.Employer;
 @Service
 public class EmployerManager implements EmployerService {
 
-	private	EmployerDao	employerDao;
-	
+	private EmployerDao employerDao;
+
 	@Autowired
 	public EmployerManager(EmployerDao employerDao) {
 		super();
@@ -26,12 +27,43 @@ public class EmployerManager implements EmployerService {
 
 	@Override
 	public DataResult<List<Employer>> getAll() {
-		
+
 		return new SuccessDataResult<List<Employer>>(this.employerDao.findAll(), "İşveren Listesi Getirildi");
 	}
 
 	@Override
 	public Result add(Employer employer) {
+
+		if (employer.getEmail().isBlank()) {
+
+			return new ErrorResult("Email bilgisi giriniz");
+
+		} else if (employer.getPassword().isBlank()) {
+
+			return new ErrorResult("Şifre Giriniz");
+
+		} else if (employer.getPasswordAgain().isBlank()) {
+
+			return new ErrorResult("Şifre Tekrarını Giriniz");
+
+		} else if (employer.getCompanyName().isBlank()) {
+
+			return new ErrorResult("Şirket Adı Giriniz");
+
+		} else if (employer.getPhoneNumber().isBlank()) {
+
+			return new ErrorResult("Telefon Giriniz");
+		} else if (employer.getWebSite().isBlank()) {
+
+			return new ErrorResult("website Giriniz");
+		}
+
+		else if (employerDao.existsByEmail(employer.getEmail())) {
+
+			return new ErrorResult("Bu mail ile kayıtlı bir kullanıcı mevcut.");
+
+		}
+
 		this.employerDao.save(employer);
 		return new SuccessResult("İşveren Sisteme eklendi");
 	}
