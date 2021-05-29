00989f8c271c28da30c77f81c56fb1578b732771
Vahdet Parlak <vahdetparlak@gmail.com>
2021-05-29 18:22:27 +0300

    mernis-update

---
+++ hrms/src/main/java/kodlamaio/hrms/core/utilities/adapters/service/CheckService.java
@@ -0,0 +1,7 @@
+package kodlamaio.hrms.core.utilities.adapters.service;
+
+import kodlamaio.hrms.entities.concretes.JobSeeker;
+
+public interface CheckService {
+	public boolean mernisVerification(JobSeeker jobSeeker);
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/core/utilities/adapters/service/FakeCheckService.java
@@ -0,0 +1,8 @@
+package kodlamaio.hrms.core.utilities.adapters.service;
+
+import kodlamaio.hrms.entities.concretes.JobSeeker;
+
+public interface FakeCheckService {
+
+	public boolean fakeMernisVerification(JobSeeker jobSeeker);
+}
