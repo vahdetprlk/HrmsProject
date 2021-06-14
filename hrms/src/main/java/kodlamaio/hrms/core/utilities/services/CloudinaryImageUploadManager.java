7b02bea04043b72d8be218a0a9498aefb8893fad
Vahdet Parlak <vahdetparlak@gmail.com>
2021-06-14 20:20:23 +0300

    Homework-11
    Update: CrossOrigin annotations added in to controllers.

--- hrms/src/main/java/kodlamaio/hrms/core/utilities/services/CloudinaryImageUploadManager.java
+++ hrms/src/main/java/kodlamaio/hrms/core/utilities/services/CloudinaryImageUploadManager.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.cloudinary.Cloudinary;
@@ -12,9 +13,9 @@ import com.cloudinary.utils.ObjectUtils;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.ErrorDataResult;
 import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
-import lombok.Data;
-@Service
 
+@Service
+	
 public class CloudinaryImageUploadManager implements ImageUploadService  {
 	private Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
 			"cloud_name", "kodlamaio-vhdt",
